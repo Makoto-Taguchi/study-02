96381cdc102f4f27f8f6fa7f28ac4dbacdc2d565
Makoto Taguchi <m.taguchi7117@gmail.com>
2021-04-29 21:13:33 +0900

    first submit

--- mynavi_sample.py
+++ mynavi_sample.py
@@ -2,10 +2,27 @@ import os
 from selenium.webdriver import Chrome, ChromeOptions
 import time
 import pandas as pd
+import datetime
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+now = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+logfile_name = './log/log_' + now + '.txt'
+outputfile_name = './log/output_{date}_{keyword}.csv'
+
+# ログファイルへの書き込みする関数
+def log(txt):
+    with open(logfile_name, 'a')as f:
+        f.write(txt + '\n')
+
+# テーブルのthタグからtargetの文字列を探して、対応するtdを返す関数
+def find_table_target_word(th_elms, td_elms, target:str):
+    for th_elm,td_elm in zip(th_elms,td_elms):
+        if th_elm.text == target:
+            return td_elm.text
+
 
 # Chromeを起動する関数
-
-
 def set_driver(driver_path, headless_flg):
     # Chromeドライバーの読み込み
     options = ChromeOptions()
@@ -23,13 +40,14 @@ def set_driver(driver_path, headless_flg):
     options.add_argument('--incognito')          # シークレットモードの設定を付与
 
     # ChromeのWebDriverオブジェクトを作成する。
-    return Chrome(executable_path=os.getcwd() + "/" + driver_path, options=options)
+    # return Chrome(executable_path=os.getcwd() + "/" + driver_path, options=options)
+    return Chrome(ChromeDriverManager().install(), options=options)
+
 
 # main処理
-
-
 def main():
-    search_keyword = "高収入"
+    search_keyword = input("検索ワードを入力してください：")
+    log(f'検索ワード："{search_keyword}"')
     # driverを起動
     if os.name == 'nt': #Windows
         driver = set_driver("chromedriver.exe", False)
@@ -38,7 +56,7 @@ def main():
     # Webサイトを開く
     driver.get("https://tenshoku.mynavi.jp/")
     time.sleep(5)
- 
+
     try:
         # ポップアップを閉じる
         driver.execute_script('document.querySelector(".karte-close").click()')
@@ -56,15 +74,69 @@ def main():
 
     # ページ終了まで繰り返し取得
     exp_name_list = []
-    # 検索結果の一番上の会社名を取得
-    name_list = driver.find_elements_by_class_name("cassetteRecruit__name")
+    exp_status_list = []
+    exp_income_list = []
+    count = 1
+    success = 0
+    fail = 0
 
-    # 1ページ分繰り返し
-    print(len(name_list))
-    for name in name_list:
-        exp_name_list.append(name.text)
-        print(name.text)
-        
+    while True:
+        name_list = driver.find_elements_by_class_name("cassetteRecruit__name")
+        status_list = driver.find_elements_by_css_selector(".cassetteRecruit__heading .labelEmploymentStatus")
+        table_list = driver.find_elements_by_css_selector(".cassetteRecruit .tableCondition")
+
+
+        # 1ページ分繰り返し
+        print(len(name_list))
+        for name, status, table in zip(name_list, status_list, table_list):
+            # 例外発生時も処理を継続する
+            try:
+                # 会社名のみ抽出する処理（スライス）
+                target ='|'
+                idx = name.text.find(target)
+                r = name.text[:idx]
+
+                exp_name_list.append(r)
+                # print(r)
+
+                exp_status_list.append(status.text)
+                # print(status.text)
+
+                income = find_table_target_word(table.find_elements_by_tag_name("th"), table.find_elements_by_tag_name("td"), "初年度年収")
+                exp_income_list.append(income)
+                # print(income)
+
+                # ログ書き込み
+                log(f'{count} 件目成功： {r}')
+                success += 1
+            except Error as e:
+                # ログ書き込み
+                log(f'{count} 件目失敗： {name.text}')
+                fail += 1
+            finally:
+                count += 1
+
+
+        # 次ページのボタンがなければ終了
+        next_page = driver.find_elements_by_css_selector(".pager__next .iconFont--arrowLeft")
+        if len(next_page) >= 1:
+            next_page_link = next_page[0].get_attribute("href")
+            driver.get(next_page_link)
+            time.sleep(5)
+        else:
+            print("最終ページです。終了します。")
+            break
+    
+    # CSVに出力
+    # df = pd.DataFrame([exp_name_list,exp_status_list,exp_income_list], columns=["会社名", "就業形態", "初年度年収"])
+    df = pd.DataFrame({"会社名":exp_name_list,
+                        "就業形態":exp_status_list,
+                        "初年度年収":exp_income_list})
+    print(df)
+    df.to_csv(outputfile_name.format(date=now,keyword=search_keyword))
+    print("CSVへの出力処理終了しました。")
+    log(f'成功件数：{success}件、失敗件数：{fail}件')
+
 
 
 # 直接起動された場合はmain()を起動(モジュールとして呼び出された場合は起動しないようにするため)
